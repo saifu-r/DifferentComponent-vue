51f52599a0caf38aeec9f1cecf4b317e6444dd11
Saifur Rahman Rubayet <saifur.rubayet@northsouth.edu>
2024-01-02 12:32:24 +0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <website-details
+      v-for="website in websites"
+      :key="website.id"
+      :title="website.title"
+      :description="website.description"
+      :link="website.link"    
+    ></website-details>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import WebsiteDetails from "./components/WebsiteResource/WebsiteDetails.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    WebsiteDetails,
+  },
+  setup() {
+    const websites = [
+      {
+        id: 1,
+        title: "Github",
+        description:
+          "GitHub is a code hosting platform for version control and collaboration",
+        link: "https://github.com/saifu-r",
+      },
+      {
+        id: 2,
+        title: "LinkedIn",
+        description:
+          "LinkedIn is the world's largest professional network on the internet.",
+        link: "https://www.linkedin.com/in/saifur-rahman-597793274/",
+      },
+    ];
+
+    return {websites}
+  },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/WebsiteResource/WebsiteDetails.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <a :href=link>Click</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    id: {
+      type: Number,
+      required: false,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+  
+});
+</script>
+
+<style scoped>
+</style>
